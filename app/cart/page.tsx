238789163a8edeb98831174a69636d20b097afd0
wizzeart <90879777+wizzeart@users.noreply.github.com>
2025-12-17 12:09:47 -0500

    Set runtime to edge for CartPage component

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,6 @@
 import CartComponent from "@/components/cart/cartComponent";
 import { getProvincias_Municipio } from "@/lib/actions/products";
-
+export const runtime = 'edge';
 
 export default async function CartPage() {
     const municipios = await getProvincias_Municipio()
