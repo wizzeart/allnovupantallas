71bf972f3a84fe456d3349cd88904ca80d3cda60
wizzeart <90879777+wizzeart@users.noreply.github.com>
2025-12-17 12:09:30 -0500

    Set runtime to edge for GarantíaPage

--- app/garantia/page.tsx
+++ app/garantia/page.tsx
@@ -1,5 +1,5 @@
 import { GarantíaComponent } from "@/components/garantia/garantia";
-
+export const runtime = 'edge';
 export default function GarantíaPage() {
     return (
         <div className="font-display bg-white dark:bg-background-dark">
